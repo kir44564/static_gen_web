ecc11f712dfecfc48a17788bbf456b80fb618be2
kir44564 <kuchmakrakirill@gmail.com>
2026-03-30 20:40:37 +0200

    TextNode to HTMLNode + typo fix

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -15,7 +15,7 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node_none_url, node_with_url)
 
     def test_eq_different_text_type(self):
-        node_plain = TextNode("Same text", TextType.PLAIN)
+        node_plain = TextNode("Same text", TextType.TEXT)
         node_bold = TextNode("Same text", TextType.BOLD)
         self.assertNotEqual(node_plain, node_bold)
 
@@ -24,6 +24,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("Text B", TextType.ITALIC)
         self.assertNotEqual(node1, node2)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,10 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
+
 class TextType(Enum):
-    PLAIN = "text"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code" 
@@ -26,3 +29,21 @@ class TextNode:
     def __repr__(self):
         url_str = self.url if self.url else "None"
         return f"TextNode({self.text}, {self.text_type.value}, {url_str})"
+
+def text_node_to_html_node(text_node):
+    type_map = {
+        TextType.TEXT: lambda n: LeafNode(None, n.text),
+        TextType.BOLD: lambda n: LeafNode("b", n.text),
+        TextType.ITALIC: lambda n: LeafNode("i", n.text),
+        TextType.CODE: lambda n: LeafNode("code", n.text),
+        TextType.LINK: lambda n: LeafNode("a", n.text, {"href": n.url}),
+        TextType.IMAGE: lambda n: LeafNode("img", "", {"src": n.url, "alt": n.text}),
+    }
+
+    if text_node.text_type not in type_map:
+        raise Exception(f"Invalid TextType: {text_node.text_type}")
+    
+    if text_node.text_type in (TextType.LINK, TextType.IMAGE) and text_node.url is None:
+        raise ValueError(f"{text_node.text_type.value} requires a URL")
+
+    return type_map[text_node.text_type](text_node)
