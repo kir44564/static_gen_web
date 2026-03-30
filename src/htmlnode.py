f3c023c26e818df5df54ebe4738d531898cf32a6
kir44564 <kuchmakrakirill@gmail.com>
2026-03-30 19:53:03 +0200

    ParentNode of HTMLNode added with Unittests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -29,4 +29,22 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
         
+        if self.children is None:
+            raise ValueError("ParentNode must hae children")
+        
+        props_str = self.props_to_html()
+
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        
+        return f"<{self.tag}{props_str}>{children_html}</{self.tag}>"
